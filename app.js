28bc95e2addc63a910ac71a6a8c0537f094dfe08
mfunk1118 <89952826+mfunk1118@users.noreply.github.com>
2021-11-02 23:27:06 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var toggle=false;
+
+document.body.querySelector(".menuIcon").addEventListener("click",function(){
+    toggle=!toggle;
+
+    if(toggle){
+        document.body.querySelector(".menu").style.right="0";
+    }else{
+        document.body.querySelector(".menu").style.right="-151px";
+    }
+
+})
